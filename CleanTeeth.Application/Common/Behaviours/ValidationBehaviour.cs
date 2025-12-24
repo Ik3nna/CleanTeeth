a47cb687bdadbab66260f780e08fc10b3fdabd85
Ik3nna <ndukwuikenna@yahoo.com>
2025-12-24 17:05:57 +0100

    ...

--- CleanTeeth.Application/Common/Behaviours/ValidationBehaviour.cs
+++ CleanTeeth.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -20,9 +20,14 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
         if (_validators.Any())
         {
             var context = new ValidationContext<TRequest>(request);
-            var failures = _validators
-                .Select(v => v.Validate(context))
-                .SelectMany(result => result.Errors)
+            
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken))
+            );
+
+            var failures = validationResults
+                .SelectMany(r => r.Errors)
                 .Where(f => f != null)
                 .ToList();
 
